a89d6b0fc828046091f3571703e45cbf66554dc0
PabloFlanmarion <pabloflanmarion@gmail.com>
2020-09-22 22:25:42 -0300

    Second commit

---
+++ src/main/java/br/com/example/ExampleJmeterApplication.java
@@ -0,0 +1,13 @@
+package br.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJmeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJmeterApplication.class, args);
+	}
+
+}
